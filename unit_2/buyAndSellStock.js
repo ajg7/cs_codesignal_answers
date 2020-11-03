467b7cd1a9141c034414434d5cafd04291e8fd54
ajg7 <ajgebara@gmail.com>
2020-11-02 23:00:50 -0600

    code signal answers

---
+++ unit_2/buyAndSellStock.js
@@ -0,0 +1,44 @@
+const buyAndSellStock = prices => {
+    let profit;
+    let maxProfit = 0;
+    prices.forEach((buy, index) => {
+        let rest = prices.slice(index + 1);
+        if (rest) {
+            let sell = Math.max(...rest);
+            if(sell > buy) {
+                profit = sell - buy;
+            }
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    })
+    return maxProfit;
+}
+
+
+console.log(buyAndSellStock([6, 3, 1, 2, 5, 4]))
+
+const alphabeticShift = inputStr => {
+    const result = [];
+    const inputArr = inputStr.split("");
+
+    for (const char of inputArr) {
+        let hex;
+        if (char === "z") {
+            hex = 96
+        } 
+        else if (char === "Z") {
+            hex = 64
+        } 
+        else {
+            hex = char.charCodeAt(0);
+        }
+        const nextHex = hex + 1;
+        result.push(String.fromCharCode(nextHex))
+    }
+    return result.join("")
+}
+
+console.log(alphabeticShift("crazy"))
+
